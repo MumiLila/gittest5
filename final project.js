e514d4cb17aa0a29630f8f1031d11979f7025b89
MumiLila <59768426+MumiLila@users.noreply.github.com>
2020-02-16 20:14:51 +0700

    third commit

---
+++ final project.js
@@ -0,0 +1,10 @@
+function changeName(){
+    var x= document.getElementById("fillname").value;
+    document.getElementById("fullname").innerHTML=x
+
+}
+
+function changeSkill(){
+    var y= document.getElementById("yuskill").value;
+    document.getElementById("footer1").innerHTML=y
+}
